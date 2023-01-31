d38a315324ececf1fbdccc5b40ce0440448f50c5
mrunaljadhav02 <112549948+mrunaljadhav02@users.noreply.github.com>
2023-01-31 14:22:17 +0530

    Assesment

---
+++ main.js
@@ -0,0 +1,17 @@
+fetch("data.json")
+.then((res) =>{
+    return res.json();
+})
+.then((data)=>{
+    console.log(data);
+    showData(data);
+});
+
+function showData(data){
+    data.forEach((ele) =>{
+        var title = document.getElementById("title");
+        var para = document.getElementById("desc");
+        title.innerText = ele.Name;
+        para.innerText = ele.ShortDesc;
+    });
+}
